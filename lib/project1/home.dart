54f33f2f2e9f32e5921e2233c90c298839e65d2a
Fazil <mfazilkp7@gmail.com>
2023-09-13 11:39:56 +0530

    main page updated

--- lib/project1/home.dart
+++ lib/project1/home.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dialysis Survey App"),
+        title: const Text("DialysisDataGuard"),
         backgroundColor: Colors.red,
         centerTitle: true,
       ),
